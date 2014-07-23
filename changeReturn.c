13b09176ff6d324b0404a21d0947dc8155a57200
Jesus Magana <maganazero5@gmail.com>
2014-07-22 21:17:00 -0700

    have 2 inputs and parrot them

--- changeReturn.c
+++ changeReturn.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-void returnChange(float inputNumber);
+//function stub
+void returnChange(double, double);
 
 //Just take in number and send to function
 int main(int argc, char* argv[]){
-  float inputNumber = 0;
-  printf("Please input amount of money: $");
-  scanf("%f", &inputNumber);
-  returnChange(inputNumber);
+  double cost = 0;
+  double given = 0;
+
+  //taking in numbers
+  printf("Please input cost: $");
+  scanf("%lf", &cost);
+  printf("\n");
+  printf("Please input amount given: $");
+  scanf("%lf", &given);
+
+  //call to function
+  returnChange(cost, given);
 	return 0;
 }
 
-void returnChange(float inputNumber){
-  printf("%f \n", inputNumber);
+//give return change
+void returnChange(double cost, double given){
+  printf("Cost:%lf amount given:%lf \n", cost, given);
 }
