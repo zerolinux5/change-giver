8edb67cb876c40a8d0a75f87921ac8a1fb3b8fb6
Jesus Magana <maganazero5@gmail.com>
2014-07-22 20:59:46 -0700

    Take input and has function stub

--- changeReturn.c
+++ changeReturn.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+void returnChange(float inputNumber);
 
 //Just take in number and send to function
 int main(int argc, char* argv[]){
-   printf("Start\n");
+  float inputNumber = 0;
+  printf("Please input amount of money: $");
+  scanf("%f", &inputNumber);
+  returnChange(inputNumber);
 	return 0;
+}
+
+void returnChange(float inputNumber){
+  printf("%f \n", inputNumber);
 }
